491f9be96a7cc3627fc722902b85edbaf731b5bc
pinky-banik <pinky.rani.banik@gmail.com>
2022-10-28 12:26:51 +0600

    server running

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const courses = require('./courses.json');
+
+
+const app = express();
+app.use(cors());
+
+const port = 5000;
+
+app.get('/',(req,res)=>{
+  res.send('E-school server running')
+})
+
+
+app.listen(port,()=>{
+  console.log(`E-school server running on port :${port}`)
+})
